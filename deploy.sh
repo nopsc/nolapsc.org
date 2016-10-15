ecf5126bec5c7a36ecdb3052ddabff374f3e1f43
New Orleans Palestinian Solidarity Committee <info@nolapsc.org>
2016-10-15 17:01:46 -0500

    Fix missing translations

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+# Copy over theme files
 rm -rf themes/palestine
 rsync -av --progress ../hugo-palestine-theme themes --exclude .git
 mv themes/hugo-palestine-theme themes/palestine
+
+# Commit and push
 git add .
 git commit -m "$1"
 git push production master
+
+# Remove theme and use symbolic link for faster development
+rm -rf themes/palestine
+ln -s ../hugo-palestine-theme themes/palestine
