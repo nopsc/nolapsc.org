7bea0a9ba9b49737f6bcbf2c61e7582e45698d72
New Orleans Palestinian Solidarity Committee <info@nolapsc.org>
2016-10-16 18:42:05 -0500

    Pull signatures count from an external server

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ rm -rf themes/palestine
 rsync -av --progress ../hugo-palestine-theme themes --exclude .git
 mv themes/hugo-palestine-theme themes/palestine
 
-# Commit and push
+# Commit and push website
 git add .
 git commit -m "$1"
 git push production master
@@ -13,3 +13,11 @@ git push production master
 # Remove theme and use symbolic link for faster development
 rm -rf themes/palestine
 ln -s ../../hugo-palestine-theme/ themes/palestine
+
+# Commit and push theme
+cd ../hugo-palestine-theme
+git add .
+git commit -m "$1"
+git push origin master
+
+cd ../nolapsc.org
